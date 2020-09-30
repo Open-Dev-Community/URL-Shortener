ad15c3abe5b2dc92dbdb9236b49b1ef3fc7d5bc5
Balaka Biswas <49288068+BALaka-18@users.noreply.github.com>
2020-09-30 12:42:36 +0530

    Update routes.py

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -4,22 +4,27 @@ from .extensions import db
 
 short_blp = Blueprint('short_blp', __name__)
 
+
 @short_blp.route('/<short_url>')
 def redirect_to(short_url):
     link_w = LinkUrl.query.filter_by(short_url=short_url).first_or_404()
     return redirect(link_w.original_url)
+
+
 @short_blp.route('/')
 def index():
     return render_template('index.html')
+
+
 @short_blp.route('/add_link', methods=['POST'])
 def add_link():
     original_url = request.form['original_url']
     link = LinkUrl(original_url=original_url)
-    # Update dataset
     db.session.add(link)
     db.session.commit()
-
     return render_template('link_added.html', new_url=link.short_url, original_url=link.original_url)
+
+
 @short_blp.errorhandler(404)
 def page_not_found(e):
     return '', 404
