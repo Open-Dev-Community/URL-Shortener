3da6f416c0eafa75989d738f7f5937596632ff90
nntin <nguyen.ngoctindaniel@gmail.com>
2020-10-05 23:28:17 +0000

    run through black codeformatter, see https://github.com/psf/black

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -4,11 +4,11 @@ from .extensions import db
 from .routes import short_blp
 
 
-def create_app(config_file='settings.py'):
+def create_app(config_file="settings.py"):
     app = Flask(__name__)
-    app.static_folder = 'static'
+    app.static_folder = "static"
     app.config.from_pyfile(config_file)
-    app.config['SESSION_COOKIE_SECURE'] = False
+    app.config["SESSION_COOKIE_SECURE"] = False
     # Initializing db
     db.init_app(app)
     # Import blueprint
--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -2,31 +2,31 @@ from flask import Blueprint, render_template, request, redirect
 from .models import LinkUrl
 from .extensions import db
 
-short_blp = Blueprint('short_blp', __name__)
+short_blp = Blueprint("short_blp", __name__)
 
 
-@short_blp.route('/<short_url>')
+@short_blp.route("/<short_url>")
 def redirect_to(short_url):
     link_w = LinkUrl.query.filter_by(short_url=short_url).first_or_404()
     return redirect(link_w.original_url)
 
 
-@short_blp.route('/')
+@short_blp.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@short_blp.route('/add_link', methods=['POST'])
+@short_blp.route("/add_link", methods=["POST"])
 def add_link():
-    original_url = request.form['original_url']
+    original_url = request.form["original_url"]
     link = LinkUrl(original_url=original_url)
     db.session.add(link)
     db.session.commit()
-    return render_template('link_added.html', 
-                           new_url=link.short_url, 
-                           original_url=link.original_url)
+    return render_template(
+        "link_added.html", new_url=link.short_url, original_url=link.original_url
+    )
 
 
 @short_blp.errorhandler(404)
 def page_not_found(e):
-    return '', 404
+    return "", 404
